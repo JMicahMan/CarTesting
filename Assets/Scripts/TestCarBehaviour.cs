3143bc7df5636e3b64aab74cf074df8fd8bc1bf8
JMicahMan <j.micah.manzanares@gmail.com>
2019-06-03 17:37:29 -0500

    Movement works

---
+++ Assets/Scripts/TestCarBehaviour.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestCarBehaviour : MonoBehaviour
+{
+    public float CarSpeed;
+    public float TurnSpeed;
+
+    public GameObject Walky;
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+
+        Vector3 Direction = (Walky.transform.position - transform.position) ;
+
+        transform.forward = Direction.normalized ;
+
+        transform.position += ((Direction * (Time.deltaTime * CarSpeed)));
+
+        Vector3 p = transform.position;
+
+        Quaternion r = transform.rotation;
+
+        if (p.y > 1)
+        {
+           p.y = 1;
+           transform.position = p;
+        }
+
+        if (p.y < 1)
+        {
+            p.y = 1;
+            transform.position = p;
+        }
+
+        if (r.x < 0)
+        {
+            r.x = 0;
+            transform.rotation = r;
+        }
+
+        if (r.x > 0)
+        {
+            r.x = 0;
+            transform.rotation = r;
+        }
+
+        if (r.z < 0)
+        {
+            r.z = 0;
+            transform.rotation = r;
+        }
+
+        if (r.z > 0)
+        {
+            r.z = 0;
+            transform.rotation = r;
+        }
+
+    }
+}
---
+++ Assets/Scripts/WalkerBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WalkerBehaviour : MonoBehaviour {
+
+    public float WalkerSpeed;
+
+    private Rigidbody rb;
+    
+	void Start ()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update ()
+    {
+        float Hori = Input.GetAxis("Horizontal");
+        float Verti = Input.GetAxis("Vertical");
+
+        Vector3 mov = new Vector3(Hori, 0.0f, Verti);
+
+        rb.AddForce(mov * WalkerSpeed);
+        
+	}
+}
